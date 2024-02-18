27597fd0f9cc4a4f3f5e0735e1994949d1391c32
sericakitty <63978546+sericakitty@users.noreply.github.com>
2024-02-18 16:58:49 +0200

    add /booklist get-endpoint

--- src/main/java/hh/sof03/bookstore/web/BookController.java
+++ src/main/java/hh/sof03/bookstore/web/BookController.java
@@ -1,13 +1,28 @@
 package hh.sof03.bookstore.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import hh.sof03.bookstore.domain.BookRepository;
+
 
 @Controller
 public class BookController {
+
+  @Autowired
+  private BookRepository repository;
   
   @GetMapping("/index")
   public String index() {
     return "index";
   }
+
+  @GetMapping("/booklist")
+  public String getAllBooks(Model model) {
+      model.addAttribute("books", repository.findAll());
+      return "booklist";
+  }
+  
 }
