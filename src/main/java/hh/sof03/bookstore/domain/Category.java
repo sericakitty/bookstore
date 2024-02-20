da5fb761194078da4f03ac7924f9d173304b8e6e
sericakitty <63978546+sericakitty@users.noreply.github.com>
2024-02-20 18:52:58 +0200

    add OneToMany relation with Book; add getter and setter.

--- src/main/java/hh/sof03/bookstore/domain/Category.java
+++ src/main/java/hh/sof03/bookstore/domain/Category.java
@@ -1,9 +1,13 @@
 package hh.sof03.bookstore.domain;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Category{
@@ -11,6 +15,10 @@ public class Category{
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long categoryId;
   private String name;
+
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+  private List<Book> books;
+
 
   public Category(String name) {
     super();
@@ -29,6 +37,14 @@ public class Category{
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public List<Book> getBooks() {
+    return books;
+  }
+
+  public void setBooks(List<Book> books) {
+    this.books = books;
   }
 
   @Override
