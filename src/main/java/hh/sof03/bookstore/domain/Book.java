57a614121722d6b7c3d3838ca34bb9de633a830c
sericakitty <63978546+sericakitty@users.noreply.github.com>
2024-02-20 14:21:02 +0200

    add toString method for command line runner

--- src/main/java/hh/sof03/bookstore/domain/Book.java
+++ src/main/java/hh/sof03/bookstore/domain/Book.java
@@ -72,4 +72,9 @@ public class Book {
     this.price = price;
   }
 
+  @Override
+  public String toString() {
+    return "Book [id=" + id + ", title=" + title + ", author=" + author + ", publicationYear=" + publicationYear + ", isbn=" + isbn + ", price=" + price + "]";
+  }
+
 }
