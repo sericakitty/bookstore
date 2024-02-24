5845e315b8a762af99cd86480a34116951a992c6
sericakitty <63978546+sericakitty@users.noreply.github.com>
2024-02-24 23:07:11 +0200

    Change @Controller to @RestController; Add /books post method to save new book from requestbody

--- src/main/java/hh/sof03/bookstore/web/BookRestController.java
+++ src/main/java/hh/sof03/bookstore/web/BookRestController.java
@@ -8,25 +8,50 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import hh.sof03.bookstore.domain.Book;
 import hh.sof03.bookstore.domain.BookRepository;
+import hh.sof03.bookstore.domain.Category;
+import hh.sof03.bookstore.domain.CategoryRepository;
 
 @CrossOrigin
-@Controller
+@RestController
 public class BookRestController {
 
   @Autowired
   private BookRepository bookRepository;
+
+  @Autowired
+  private CategoryRepository categoryRepository;
   
   @GetMapping("/books")
-  public @ResponseBody List<Book> bookListRest() {
+  public List<Book> bookListRest() {
     return (List<Book>) bookRepository.findAll();
   }
 
   @GetMapping("/books/{id}")
-  public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+  public Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
     return bookRepository.findById(bookId);
   }
+
+  @PostMapping("/books")
+  public Book saveBookRest(@RequestBody Book bookObject) {
+    Book book = new Book();
+    book.setTitle(bookObject.getTitle());
+    book.setAuthor(bookObject.getAuthor());
+    book.setPublicationYear(bookObject.getPublicationYear());
+    book.setIsbn(bookObject.getIsbn());
+    book.setPrice(bookObject.getPrice());
+    Category category = categoryRepository.findByName(bookObject.getCategory().getName());
+    book.setCategory(category);
+    bookRepository.save(book);
+
+    return book;
+  }  
+  
+
 }
