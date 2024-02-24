fc0edfd50606fd37e27a3a4272bb33811c1ac35d
sericakitty <63978546+sericakitty@users.noreply.github.com>
2024-02-24 16:05:40 +0200

    Created /books/id -Get method which returns book by id.

--- src/main/java/hh/sof03/bookstore/web/BookRestController.java
+++ src/main/java/hh/sof03/bookstore/web/BookRestController.java
@@ -1,12 +1,14 @@
 package hh.sof03.bookstore.web;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import hh.sof03.bookstore.domain.Book;
 import hh.sof03.bookstore.domain.BookRepository;
@@ -21,5 +23,10 @@ public class BookRestController {
   @GetMapping("/books")
   public @ResponseBody List<Book> bookListRest() {
     return (List<Book>) bookRepository.findAll();
+  }
+
+  @GetMapping("/books/{id}")
+  public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+    return bookRepository.findById(bookId);
   }
 }
