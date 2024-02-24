451510cda8ea16448dc57163a7d9f00ed127fe4f
sericakitty <63978546+sericakitty@users.noreply.github.com>
2024-02-24 15:46:54 +0200

    add JsonIgnoreProperties annotation

--- src/main/java/hh/sof03/bookstore/domain/Book.java
+++ src/main/java/hh/sof03/bookstore/domain/Book.java
@@ -1,5 +1,7 @@
 package hh.sof03.bookstore.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,6 +21,7 @@ public class Book {
   private double price;
 
   @ManyToOne
+  @JsonIgnoreProperties("books")
   @JoinColumn(name = "categoryid")
   private Category category;
 
