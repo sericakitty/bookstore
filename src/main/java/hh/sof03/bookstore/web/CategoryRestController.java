af459d9e3446c35d88353e06c0c6c20232f1f4c4
sericakitty <63978546+sericakitty@users.noreply.github.com>
2024-02-25 00:25:27 +0200

    add /categories/{id} get method which returns category by id, and books that fall under the category.

--- src/main/java/hh/sof03/bookstore/web/CategoryRestController.java
+++ src/main/java/hh/sof03/bookstore/web/CategoryRestController.java
@@ -3,6 +3,7 @@ package hh.sof03.bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,17 @@ public class CategoryRestController {
   @GetMapping("/categories")
   public Iterable<Category> categoryListRest() {
     return categoryRepository.findAll();
+  }
+
+  @GetMapping("/categories/{id}")
+  public Category findCategoryRest(@PathVariable("id") Long categoryId) {
+    Category categoryExists = categoryRepository.findById(categoryId).orElse(null);
+
+    if (categoryExists == null) {
+      throw new IllegalArgumentException("Category not found");
+    }
+
+    return categoryExists;
   }
 
   @PostMapping("/categories")
