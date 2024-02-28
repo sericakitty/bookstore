14021824ad32d12c88faf108a38358f93218fde2
sericakitty <63978546+sericakitty@users.noreply.github.com>
2024-02-28 14:05:10 +0200

    Implement user authentication and password encryption using BCrypt

--- src/main/java/hh/sof03/bookstore/WebSecurityConfig.java
+++ src/main/java/hh/sof03/bookstore/WebSecurityConfig.java
@@ -2,19 +2,26 @@ package hh.sof03.bookstore;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import hh.sof03.bookstore.web.UserDetailServiceImpl;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig  {
+
+  @Autowired
+  private UserDetailServiceImpl userDetailsService;
 
   @Bean
   public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -31,5 +38,10 @@ public class WebSecurityConfig  {
       )
       .logout(logout -> logout.permitAll());
     return http.build();
+  }
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+      auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
   }
 }
