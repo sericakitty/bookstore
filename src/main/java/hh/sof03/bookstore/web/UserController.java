2991e4f4d0b56fc3ca56f2c790257f02e849efd1
sericakitty <63978546+sericakitty@users.noreply.github.com>
2024-03-01 16:04:33 +0200

    add /signup Get enpoint to fetch signup form

--- src/main/java/hh/sof03/bookstore/web/UserController.java
+++ src/main/java/hh/sof03/bookstore/web/UserController.java
@@ -1,8 +1,10 @@
 package hh.sof03.bookstore.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import hh.sof03.bookstore.domain.SignupForm;
 
 @Controller
 public class UserController {
@@ -12,5 +14,10 @@ public class UserController {
     return "login";
   }
 
+  @GetMapping("/signup")
+  public String addBook(Model model) {
+    model.addAttribute("signupform", new SignupForm());
+    return "signup";
+  }
 
 }
